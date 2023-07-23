99b40837191b75c22d5f8c01f127e7c608732327
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-23 18:54:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import pandas as pd
 age = st.slider('How old are you?', 0, 130, 25)
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
+    'second column': [10*age, 20*age, 30*age, 40*age],
 }))
 
-age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
 # This is a sample Python script.
 
