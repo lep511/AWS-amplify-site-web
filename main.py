d9d1c504ca38fe5120448f402a9573c9779a2daa
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-23 19:02:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import pandas as pd
 df = pd.read_csv('https://ckan.montevideo.gub.uy/dataset/159475cc-6584-48d3-961c-b6fa71e14cba/resource/fd845194-4cc7-43ea-bc06-00bbfb26f6f4/download/bprumd85rjyqcuqprxpplw.csv')
 st.write(len(df))
 
-st.write(df.head(20))
+
 
 age = st.slider('How old are you?', 0, 130, 25)
+
+st.write(df.head(age))
+
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10*age, 20*age, 30*age, 40*age],
 }))
+
+
 
 st.write("I'm ", age, 'years old')
 # This is a sample Python script.
