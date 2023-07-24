8c0d4d73d4f0755818a01fe7415f673479acd6a8
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-24 11:23:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-@st.cache_data
-def load_data():
-    df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
-    return df
-
-
-df = load_data()
-
-st.table(df)
+st.map(df)
